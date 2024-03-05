5f1f13e317daf45691274849c36954da6cedf056
Jonas Lagoni <jonas-lt@live.dk>
2024-03-05 15:05:14 +0100

    feat: update release config

--- release.config.js
+++ release.config.js
@@ -5,20 +5,5 @@
 module.exports = {
   branches: ['setup_release_for_cli'],
   extends: 'semantic-release-monorepo',
-  plugins: [
-    '@semantic-release/npm',
-    '@semantic-release/github',
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'conventionalcommits'
-      }
-    ],
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        preset: 'conventionalcommits'
-      }
-    ]
-  ]
+  plugins: ['@semantic-release/npm', '@semantic-release/github']
 };
