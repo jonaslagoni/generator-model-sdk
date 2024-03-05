64ab4f35160f54a5bb9fad6113400069b456a7a1
Jonas Lagoni <jonas-lt@live.dk>
2024-03-05 14:33:32 +0100

    feat: update release config

---
+++ release.config.cjs
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ['master', 'next', 'setup_release_for_cli'],
+  extends: 'semantic-release-monorepo',
+  plugins: [
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits'
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits'
+      }
+    ]
+  ]
+};
